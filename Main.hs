53d194bae7b574f25d2b1a12014504c9fbf6c567
M42 <mromang08@gmail.com>
2015-06-16 19:21:49 +0200

    Praeludium I Working example using the arpeggiator, a bass line and a random sequence of chords.

--- Main.hs
+++ Main.hs
@@ -40,5 +40,30 @@ randomSeqWithBass = do let bas = 60
                        writeMidi "main.midi" music
 
 
+randomPraeludium :: IO ()
+randomPraeludium = do  let bas = 60
+                       let bass = 60-12*2
+                       let dur = wn
+
+                       -- Chords
+                       d <- chords 6 I
+                       let c = d ++ [V,I]
+                       let m = map (scaleChord Major) c
+                       let n = map (\x -> scaleNote Major x bas) c
+                       let musicl = map (flip (uncurry cchord) dur) (zip m n)
+                       let music = foldl (:+:) (rest 0) musicl
+
+                       -- Bassline
+                       let bmusic = bassline bass wn c
+
+                       -- Arpeggio
+                       let arp = map (flip (uncurry arpeggio) praeludium) (zip n m)
+                       let arpmusic = foldl (:+:) (rest 0) arp
+
+                       let commusic = music :=: arpmusic :=: bmusic
+                       print c
+                       play commusic
+                       writeMidi "main.midi" music
+
 main :: IO ()
-main = randomSeqWithBass
+main = randomPraeludium
