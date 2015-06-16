1c8d096f334299a7138c92d17aab42ba31ea8638
M42 <mromang08@gmail.com>
2015-06-16 14:59:38 +0200

    Random sequence of chords

--- Main.hs
+++ Main.hs
@@ -5,12 +5,16 @@ import RandomChord
 import Chords
 import Scale
 
+randomSequence :: IO ()
+randomSequence = do d <- chords 6 I
+                    let c = d ++ [V,I]
+                    let m = map (scaleChord Major) c
+                    let n = map (\d -> scaleNote Major d 40) c
+                    let musicl = map (flip (uncurry cchord) wn) (zip m n)
+                    let music = foldl (:+:) (rest 0) musicl
+                    print c
+                    play music
+                    writeMidi "main.midi" music
+
 main :: IO ()
-main = do c <- chords 6 I
-          let m = map (scaleChord Major) c
-          let n = map (\d -> scaleNote Major d 40) c
-          let musicl = map (flip (uncurry cchord) wn) (zip m n)
-          let music = foldl (:+:) (rest 0) musicl
-          print music
-          play music
-          writeMidi "main.midi" music
+main = randomSequence
