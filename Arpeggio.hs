6a23252108ab1215e3125242a6086ec286f89cf2
M42 <mromang08@gmail.com>
2015-06-16 19:07:54 +0200

    Arpeggiator

---
+++ Arpeggio.hs
@@ -0,0 +1,15 @@
+module Arpeggio where
+
+import Euterpea
+import Chords
+
+type Motif = [(Int,Dur)]
+
+praeludium :: Motif
+praeludium = zip [0,1,2,3,4,2,3,4] [en,en..]
+
+
+arpeggio :: AbsPitch -> ChordMode -> Motif -> Music Pitch
+arpeggio base mode motif = line $ develop motif
+    where develop = map (\(a,b) -> Prim $ Note b $ pitch $ posToNote a)
+          posToNote = ((cycle $ chordAbsBase mode base) !!)
