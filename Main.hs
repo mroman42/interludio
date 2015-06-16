0b84ed8a78953dd4b75aa9fd92a9546cab0b170d
M42 <mromang08@gmail.com>
2015-06-16 15:55:20 +0200

    Bassline main example

--- Main.hs
+++ Main.hs
@@ -4,17 +4,39 @@ import Euterpea
 import RandomChord
 import Chords
 import Scale
+import Bassline
 
 randomSequence :: IO ()
 randomSequence = do d <- chords 6 I
                     let c = d ++ [V,I]
                     let m = map (scaleChord Major) c
-                    let n = map (\d -> scaleNote Major d 40) c
+                    let n = map (\x -> scaleNote Major x 40) c
                     let musicl = map (flip (uncurry cchord) wn) (zip m n)
                     let music = foldl (:+:) (rest 0) musicl
                     print c
                     play music
                     writeMidi "main.midi" music
 
+randomSeqWithBass :: IO ()
+randomSeqWithBass = do let bas = 60
+                       let bass = 60-12*2
+
+                       -- Chords
+                       d <- chords 6 I
+                       let c = d ++ [V,I]
+                       let m = map (scaleChord Major) c
+                       let n = map (\x -> scaleNote Major x bas) c
+                       let musicl = map (flip (uncurry cchord) wn) (zip m n)
+                       let music = foldl (:+:) (rest 0) musicl
+                    
+                       -- Bassline
+                       let b = bassline bass wn d
+                       
+                       let commusic = music :=: b
+                       print c
+                       play commusic
+                       writeMidi "main.midi" music
+
+
 main :: IO ()
-main = randomSequence
+main = randomSeqWithBass
