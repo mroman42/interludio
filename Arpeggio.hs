bf4703dbeed1119bb9ee53b280176c98e425ef08
M42 <mromang08@gmail.com>
2015-06-24 23:39:02 +0200

    Minor changes

--- Arpeggio.hs
+++ Arpeggio.hs
@@ -8,7 +8,6 @@ type Motif = [(Int,Dur)]
 praeludium :: Motif
 praeludium = zip [0,1,2,3,4,2,3,4] [en,en..]
 
-
 arpeggio :: AbsPitch -> ChordMode -> Motif -> Music Pitch
 arpeggio base mode motif = line $ develop motif
     where develop = map (\(a,b) -> Prim $ Note b $ pitch $ posToNote a)
