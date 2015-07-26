b0ef26a4bd8b6f7e266a1ec28dd2983b0b938a0b
M42 <mromang08@gmail.com>
2015-07-26 13:02:56 +0200

    Documents Bassline

--- Bassline.hs
+++ Bassline.hs
@@ -4,6 +4,9 @@ import Euterpea
 import Chords
 import Scale
 
+
+-- | Given a base note and a duration, creates a melodic
+--   line with the roots of the chords.
 bassline :: AbsPitch -> Dur -> [Degree] -> Music Pitch
 bassline n d ch = line $ map tr ch
     where tr = Prim . (Note d) . pitch . (+n) . fromEnum . sn
