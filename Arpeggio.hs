67fca59df05ca3a167023149b410c3d736f0f046
M42 <mromang08@gmail.com>
2015-07-26 13:02:38 +0200

    Documents arpeggio.hs

--- Arpeggio.hs
+++ Arpeggio.hs
@@ -5,10 +5,15 @@ import Chords
 
 type Motif = [(Int,Dur)]
 
-praeludium :: Motif
-praeludium = zip [0,1,2,3,4,2,3,4] [en,en..]
-
+-- | Develops a motif upon a given chord and base note.
 arpeggio :: AbsPitch -> ChordMode -> Motif -> Music Pitch
 arpeggio base mode motif = line $ develop motif
     where develop = map (\(a,b) -> Prim $ Note b $ pitch $ posToNote a)
           posToNote = ((cycle $ chordAbsBase mode base) !!)
+
+
+-- | Motif example. Taken from Praeludium 1, from The Well-Tempered Clavier
+praeludium :: Motif
+praeludium = zip [0,1,2,3,4,2,3,4] [en,en..]
+
+
